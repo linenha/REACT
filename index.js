20b361e3e47e9c5a992e2772ad8e5a1818886188
linenha <32948592+linenha@users.noreply.github.com>
2020-03-17 23:12:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Olá Mundo! By Aline Rakelli</h1>;
+console.log(element);
+ReactDOM.render(element, document.getElementById('root'));
